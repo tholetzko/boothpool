b67834131f77684100eb7d94dfbe5e6de5c3b76c
Tobias <tholetzko@users.noreply.github.com>
2017-05-24 16:35:24 -0500

    Created directories

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,2 +1,4 @@
 class Journey < ApplicationRecord
+  belongs_to :organizer, :class_name => "User"
+  has_many :comments, :dependent => :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+  has_many :comments, :dependent => :destroy
+  has_many :journeys, :foreign_key => "organizer_id", :dependent => :destroy
 end
